a33d2267dce19e4bcb7b3182220fcbd9587a5117
Adrian Chen <adrianchen@uvic.ca>
2025-10-04 19:20:01 -0700

    fixed file names.

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,6 +1,6 @@
-import './App.css'
+import './styles/App.css'
 import Heatmap from './components/Heatmap.jsx'
-import NavBar from './components/navbar.jsx'
+import NavBar from './components/NavBar.jsx'
 import Home from './pages/Home.jsx'
 
 
--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,7 +1,7 @@
 // Home.jsx
 import React from 'react';
 import HeatMap from '../components/Heatmap.jsx'
-import './Home.css';
+import '../styles/Home.css';
 
 export default function Home() {
   return (
