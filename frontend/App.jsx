faba7e037dda53067ef361d3beb3978c224232f6
Nate Morley <nathanmorley@uvic.ca>
2025-10-04 18:59:49 -0700

    fixed bad calls

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Heatmap from './components/Heatmap.jsx'
 import NavBar from './components/navbar.jsx'
-import Home from './pages/home.jsx'
+import Home from './pages/Home.jsx'
 
 
 function App() {
