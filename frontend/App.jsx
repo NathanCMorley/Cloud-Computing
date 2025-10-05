5f42395ff3a4fb8650fef4c15b5b4e1cd86880ac
Adrian Chen <adrianchen@uvic.ca>
2025-10-04 20:14:44 -0700

    Added navbar and page routing.

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,21 +1,22 @@
 import './styles/App.css'
-import Heatmap from './components/Heatmap.jsx'
 import NavBar from './components/NavBar.jsx'
 import Home from './pages/Home.jsx'
-
+import Map from './pages/Map.jsx'
+import About from './pages/About.jsx'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
-
   return (
     <>
       <NavBar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/map" element={<Map />} />
+      </Routes>
     </>
-
-
-
-  )
-  
+  );
 }
 
 export default App
--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "leaflet/dist/leaflet.css"; // Leaflet styles first
-import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </Router>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
